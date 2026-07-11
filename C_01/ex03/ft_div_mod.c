a1724d67a60ba95230d919d52e92a4e6bc33c09b
sirfranciscofrank <sirfrancisco2003@gmail.com>
2026-07-11 21:14:36 +0700

    Finished ex03

---
+++ C_01/ex03/ft_div_mod.c
@@ -0,0 +1,17 @@
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+    *div = a / b;
+    *mod = a % b; 
+}
+#include <stdio.h>
+int main(void)
+{
+	int div;
+	int mod;
+ 
+	ft_div_mod(4, 2, &div, &mod);
+	printf("17 / 5 -> div = %d (expected 2)\n", div);
+	printf("17 %% 5 -> mod = %d (expected 0)\n", mod);
+
+    return (0);
+}

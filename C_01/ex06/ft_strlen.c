91dc1e346e8a4589d7e40fde647d7ac36159a489
sirfranciscofrank <sirfrancisco2003@gmail.com>
2026-07-11 22:01:22 +0700

    Finsih ex05 and ex06

---
+++ C_01/ex06/ft_strlen.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Length of \"Hello\" = %d", ft_strlen("Hello"));
+
+    return (0);
+}
